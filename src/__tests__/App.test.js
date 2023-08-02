fa40ac84c8f3bb1dd310314eea401d15207b6275
Tracy <tracy.kagiri@moringa.student.com>
2023-08-02 11:32:34 +0300

    completed

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,8 @@
-test("Test passing", () => {
-  return new Promise((resolve, reject) => {
-    resolve(true);
-  });
+import { render, screen } from '@testing-library/react';
+import App from './components/App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
